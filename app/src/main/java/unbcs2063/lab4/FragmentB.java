a7bf6978b21e765fa1958573e4399460dba6eefc
timsonier <timothy.sonier@gmail.com>
2016-02-04 01:57:23 -0400

    Initial commit

---
+++ app/src/main/java/unbcs2063/lab4/FragmentB.java
@@ -0,0 +1,17 @@
+package unbcs2063.lab4;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Tim on 2016-02-04.
+ */
+public class FragmentB extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.activity_fragment_b, container, false);
+    }
+}
